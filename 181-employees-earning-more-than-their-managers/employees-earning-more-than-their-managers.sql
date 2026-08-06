8aca3c27d7b76abe6c83d54005fd112e19e6684f
M-zz-k <mizbakhanun2006@gmail.com>
2026-08-06 15:31:24 +0530

    Time: 227 ms (75.62%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-Select e.name as Employee from Employee as e join Employee as n on e.managerId = n.id and e.salary > n.salary;
+Select e.name as Employee from Employee as e join Employee as n on e.managerId = n.id where e.salary > n.salary;
