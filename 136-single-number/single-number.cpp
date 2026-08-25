3053fa017d45e0a29d0e7f4244723b1a1166dc58
M-zz-k <mizbakhanun2006@gmail.com>
2026-08-25 21:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (84.07%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,14 +1,9 @@
-#include <unordered_set>
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       unordered_set <int> seen;
-       for (int num : nums){
-           if ((seen.count(num)) > 0){ // check if a number appears more than once
-            seen.erase(num);           // if that number is present then delete from set
-            
-           }else{
-           seen.insert(num);} //if not then insert it into the set
-       } return *seen.begin(); // the only number in the set if the non duplicate number
-    }
+        int result = 0;
+        for (int num : nums){
+            result = result ^ num;
+        }
+        return result;}
 };
