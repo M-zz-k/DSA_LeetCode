b527c871f3d67ff41a1adb19857dbe691a52e13c
M-zz-k <mizbakhanun2006@gmail.com>
2026-09-07 11:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (54.34%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -5,24 +5,20 @@ class Solution {
 public:
     void nextPermutation(std::vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2;
+        int i = n-2;
 
-        // Step 1: Find the first decreasing element from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        while (i >= 0 && nums[i] >= nums[i+1]){
             i--;
         }
 
-        // Step 2 & 3: If a pivot index is found, find successor and swap
-        if (i >= 0) {
+        if(i >= 0){
             int j = n - 1;
-            while (nums[j] <= nums[i]) {
+            while(nums[j] <= nums[i]){
                 j--;
             }
-            std::swap(nums[i], nums[j]);
+            swap(nums[j] , nums[i]);
         }
-
-        // Step 4: Reverse the suffix elements after index i
-        std::reverse(nums.begin() + i + 1, nums.end());
+        reverse(nums.begin() + i +1,nums.end());
     }
 };
 
